6f91bff6ad8a136af22cfcdfb5ccf8bbd4f96fcb
brianbassey37 <brianbassey37@gmail.com>
2022-11-30 14:39:07 -0800

    mysql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all databases of the MySQL server.
-SHOW DATABASES;
+-- Lists the privileges for some users on the server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
