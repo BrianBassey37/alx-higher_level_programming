9cf7d8d7ff191467c8ec0e17af0391ccf95d80a0
brianbassey37 <brianbassey37@gmail.com>
2023-02-18 00:09:38 -0800

    add all files

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-# Changes the name of the State object with id = 2 to
-# New Mexico in the database hbtn_0e_6_usa.
-# Usage: ./12-model_state_update_id_2.py <mysql username> /
-#                                        <mysql password> /
-#                                        <database name>
+
+""" Write a script that changes the name of a State object from the
+database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username, mysql password and
+database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state - from model_state
+import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+Change the name of the State where id = 2 to New Mexico
+Your code should not be executed when imported """
+
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+import sqlalchemy
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import session, sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
+    getState = session.query(State).filter_by(id=2).first()
+    getState.name = "New Mexico"
+    """ print("{}".format(str(getState))) """
     session.commit()
-
+    session.close()
