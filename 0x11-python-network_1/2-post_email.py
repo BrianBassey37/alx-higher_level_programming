32e3fde2af30db188fdea7b41bf3376ef5a43987
brianbassey37 <brianbassey37@gmail.com>
2023-02-18 12:00:12 -0800

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST request to the
-passed URL with the email as a parameter, and displays the body of the response
-(decoded in utf-8)"""
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
+"""
+import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
-    from urllib import parse, request
-    from sys import argv
-    email = parse.urlencode({"email": argv[2]}).encode('utf-8')
-    url = request.Request(argv[1], email)
-    with request.urlopen(url) as response:
-        req = response.read().decode('utf-8')
-        print("{}".format(req))
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
